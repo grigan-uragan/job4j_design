f1e7fb4d879d0469c2068cb4ec84159aff0c1afc
grigan-Uragan <prischepny@mail.ru>
2020-09-20 01:41:11 +0300

    1. Создать элементарную структуру дерева [#355356]

---
+++ Chapter_001/src/main/java/ru/job4j/collection/SimpleTree.java
@@ -0,0 +1,29 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public interface SimpleTree<E> {
+
+    boolean add(E parent, E child);
+
+    Optional<Node<E>> findBy(E value);
+
+    class Node<E> {
+        private final E value;
+        private final List<Node<E>> children = new ArrayList<>();
+
+        public Node(E value) {
+            this.value = value;
+        }
+
+        public List<Node<E>> getChildren() {
+            return children;
+        }
+
+        public E getValue() {
+            return value;
+        }
+    }
+}
---
+++ Chapter_001/src/main/java/ru/job4j/collection/Tree.java
@@ -0,0 +1,41 @@
+package ru.job4j.collection;
+
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.Queue;
+
+public class Tree<E> implements SimpleTree<E> {
+    private final Node<E> root;
+
+    public Tree(final E root) {
+        this.root = new Node<>(root);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        Optional<Node<E>> par = findBy(parent);
+        if (par.isPresent()) {
+           if (!par.get().getChildren().contains(child)) {
+               par.get().getChildren().add(new Node<>(child));
+               return true;
+           }
+        }
+        return false;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> result = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> element = data.poll();
+            if (element.getValue().equals(value)) {
+                result = Optional.of(element);
+                break;
+            }
+            data.addAll(element.getChildren());
+        }
+        return result;
+    }
+}
