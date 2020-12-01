092b911516bcd1aa2a22887d8fd8f17c9ee5cfa7
grigan-Uragan <prischepny@mail.ru>
2020-12-01 23:24:23 +0300

    2. Анализ доступности сервера. [#355394]

--- Chapter_002/src/main/java/io/Analizy.java
+++ Chapter_002/src/main/java/io/Analizy.java
@@ -1,44 +1,35 @@
 package io;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Analizy {
 
-    private String startRecord;
-    private boolean needRecord = false;
-
     public void unavailable(String source, String target) {
-        File file = new File(target);
-        file.delete();
-        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
-          reader.lines()
-                  .filter(s -> !s.isEmpty())
-                  .map(s -> s.split("\\s"))
-                  .forEach(strings -> fallingStart(strings, file));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void fallingStart(String[] strings, File fileName) {
-        if (strings[0].equals("500") || strings[0].equals("400")) {
-            if (!needRecord) {
-                needRecord = true;
-                startRecord = strings[1];
-            }
-        } else {
-            if (needRecord) {
-                String stopRecord = strings[1];
-                needRecord = false;
-                 writeFallingDownTime(startRecord
-                         + ";" + stopRecord, fileName);
-            }
-        }
-    }
-
-    private void writeFallingDownTime(String diapozone, File fileName) {
-        try (PrintWriter writer = new PrintWriter(new FileWriter(fileName, true))) {
-            writer.println(diapozone);
+        boolean needRecord = false;
+        String startRecord = "";
+        String stopRecord;
+        try (BufferedReader reader = new BufferedReader(new FileReader(source));
+             PrintWriter writer = new PrintWriter(new FileWriter(target))) {
+            String log;
+             while (reader.ready()) {
+                log  = reader.readLine();
+                String[] strings = log.split("\\s");
+                 if (strings[0].equals("500") || strings[0].equals("400")) {
+                     if (!needRecord) {
+                         needRecord = true;
+                         startRecord = strings[1];
+                     }
+                 } else {
+                     if (needRecord) {
+                         stopRecord = strings[1];
+                         needRecord = false;
+                         writer.println(startRecord + ";" + stopRecord);
+                     }
+                 }
+             }
         } catch (Exception e) {
             e.printStackTrace();
         }
