bf7b00f8d169704a5715c251b219a97e9f751c14
grigan-Uragan <prischepny@mail.ru>
2020-12-03 20:56:55 +0300

    3.0. Тестирование IO [#355385]

--- Chapter_002/src/main/java/io/Abuse.java
+++ Chapter_002/src/main/java/io/Abuse.java
@@ -5,13 +5,16 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class Abuse {
-    public static void drop(String source, String target, List<String> words) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(source));
-        PrintWriter writer = new PrintWriter(new FileWriter(target));
-        reader.lines()
-                .flatMap(s -> Stream.of(s.split("\\s")))
-                .filter(word -> !words.contains(word))
-                .map(word -> word + " ")
-                .forEach(writer::print);
+    public static void drop(String source, String target, List<String> words) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(source));
+            PrintWriter writer = new PrintWriter(new FileWriter(target))) {
+            reader.lines()
+                    .flatMap(s -> Stream.of(s.split("\\s")))
+                    .filter(word -> !words.contains(word))
+                    .map(word -> word + " ")
+                    .forEach(writer::print);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- Chapter_002/src/test/java/io/AnalizyTest.java
+++ Chapter_002/src/test/java/io/AnalizyTest.java
@@ -1,14 +1,22 @@
 package io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.File;
+import javax.lang.model.util.ElementScanner6;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class AnalizyTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
     public void whenServerDoNotFailedThenReportEmpty() {
@@ -22,12 +30,27 @@ public class AnalizyTest {
     }
 
     @Test
-    public void whenServerFallingTwiceThenTwoLinesInReport() {
-          Analizy analizy = new Analizy();
-          analizy.unavailable("/Users/Admin/IdeaProjects/job4j_design/ServerFailedLog.txt",
-                  "report.csv");
-          BufferedReadFile file = new BufferedReadFile();
-          List<String> result = file.readLogFile("report.csv");
+    public void whenServerFallingTwiceThenTwoLinesInReport() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        Analizy analizy = new Analizy();
+        List<String> result = new ArrayList<>();
+        try (PrintWriter writer = new PrintWriter(new FileWriter(source))) {
+            writer.println("200 10:56:01\n"
+                    + "500 10:57:01\n"
+                    + "400 10:58:01\n"
+                    + "200 10:59:01\n"
+                    + "500 11:01:02\n"
+                    + "200 11:02:02");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
+            result = reader.lines().collect(Collectors.toList());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
           assertThat(result.size(), is(2));
     }
 }
