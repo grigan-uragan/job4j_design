a55fc58348da1217306dbcc686b3eb3f7b635f45
grigan-Uragan <prischepny@mail.ru>
2021-01-09 17:40:59 +0300

    Хранилище машин [#355324]

--- Chapter_003/car_project.sql
+++ Chapter_003/car_project.sql
@@ -31,7 +31,14 @@ from car_project c left join car_body b on c.body_id = b.body_id
 left join car_engine e on c.engine_id = e.engine_id
 left join car_transmission t on c.transmission_id = t.transmission_id;
 
-select b.body_name as version, e.engine_name as engine, t.transmission_name as KPP
-from car_project c full join car_body b on c.body_id = b.body_id
-full join car_engine e on c.engine_id = e.engine_id
-full join car_transmission t on c.transmission_id = t.transmission_id where c.car_project_name is null;
+select b.body_name
+from car_project c right join car_body b on c.body_id = b.body_id
+where c.car_project_id is null;
+
+select e.engine_name
+from car_project c right join car_engine e on c.engine_id = e.engine_id
+where c.car_project_id is null;
+
+select t.transmission_name
+from car_project c right join car_transmission t on c.transmission_id = t.transmission_id
+where c.car_project_id is null;
